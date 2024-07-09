0d6a03ad464d14ab7e17a56586ae2f4467d36ea9
nicoContigliani <nico.contigliani>
2024-07-09 15:34:30 -0300

    todo

--- src/components/checkboxmaps/Checkbox.tsx
+++ src/components/checkboxmaps/Checkbox.tsx
@@ -1,27 +1,4 @@
 
-
-// import React from 'react';
-// import { Checkbox } from 'antd';
-
-// const Checkboxs = (props: any) => {
-//     const { name, classN, optionsTecnology, selectedValue, setSelectedValue } = props
-
-//     const handleChange = (checkedValues: any) => {
-//         setSelectedValue(checkedValues);
-//     };
-
-//     return (
-//         <Checkbox.Group
-//             className={classN}
-//             options={optionsTecnology}
-//             value={selectedValue}
-//             onChange={handleChange}
-//         />
-//     );
-// };
-
-// export default Checkboxs;
-import React from 'react';
 import { Radio } from 'antd';
 
 const RadioGroup = (props: any) => {
