5e9d7c56c28827af68925bde3b684614ac1d83c7
nicoContigliani <nico.contigliani>
2024-07-13 12:20:20 -0300

    todo

--- src/components/Map/Maps.tsx
+++ src/components/Map/Maps.tsx
@@ -1,168 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import { generateNumbersRandom } from '../../utils/generateNumberRandom.utils';
-// import Asks from './Asks/Asks';
-// import '../../assets/styles/main.scss';
-// import Checkboxs from '../checkboxmaps/Checkbox';
-// import Asnwers from './Aswers/Asnwers';
-// import { Card, Col, Row } from 'antd';
-// import Buttons from '../Buttons/Button';
-// import Rates from '../Rates/Rates';
-// import moment from 'moment';
-// import { writteLocalStorage } from '../../services/writteLocalStorage.services';
-// import { formaterDataAndTime } from '../../utils/formaterDataAndTime.utils';
-
-// const Maps = (props: any | undefined) => {
-//     const { dataMapAll, getTodo, tecnoData, levelData } = props;
-//     console.log("🚀 ~ Maps ~ dataMapAll:", dataMapAll)
-
-//     const [dataMapReturn, setDataMapReturn] = useState<any | undefined | any[]>();
-//     const [selectedValues, setSelectedValues] = useState<{ [key: string]: any }>({});
-
-//     const [resetMap, setResetMap] = useState<boolean>(false)
-
-//     if (!dataMapAll || !Array.isArray(dataMapAll)) {
-//         return <div>No data available</div>;
-//     }
-
-//     useEffect(() => {
-//         const todo: any | any[] | undefined = Array.from({ length: getTodo }, () => {
-//             try {
-//                 const dataReturn = generateNumbersRandom(0, dataMapAll.length ?? 0);
-//                 return dataMapAll[dataReturn];
-//             } catch (error) {
-//                 console.log("🚀 ~ const todo ~ error:", error)
-//             }
-//         });
-
-//         setDataMapReturn(todo);
-//     }, [dataMapAll, getTodo]);
-
-//     const handleRadioChange = (name: string, value: any) => {
-//         setSelectedValues((prevValues) => ({
-//             ...prevValues,
-//             [name]: value,
-//         }));
-//     };
-
-//     const resetButton = () => {
-//         const todo: any | any[] | undefined = Array.from({ length: getTodo }, () => {
-//             try {
-//                 const dataReturn = generateNumbersRandom(0, dataMapAll.length ?? 0);
-//                 return dataMapAll[dataReturn];
-//             } catch (error) {
-//                 console.log("🚀 ~ const todo ~ error:", error)
-//             }
-//         });
-
-//         setDataMapReturn(todo);
-//     }
-
-//     const saveAsk = (props: any) => {
-//         let element: any = { ...props }
-//         element.tecnology = tecnoData || undefined
-//         element.levelData = levelData || undefined
-//         console.log("🚀 ~ saveAsk ~ element:", element)
-
-//         const now = moment();
-//         const formattedDateTime = formaterDataAndTime(now, 'middel')
-//         writteLocalStorage(element, formattedDateTime)
-//     }
-
-//     const getItDelete = () => {
-//         // Función para manejar la eliminación
-//     }
-
-//     const todoS = (dataR: any) => {
-//         console.log("🚀 ~ todoS ~ dataR:", dataR)
-//         // if (dataR === undefined) setResetMap(true)
-//         if(dataR) resetButton()
-
-//         return true
-//     }
-
-
-//     useEffect(() => {
-
-//     }, [])
-
-
-
-
-//     return (
-//         <div className='checkClass'>
-//             {
-//                 (dataMapReturn !== undefined) ? dataMapReturn.map((item: any, index: any) => (
-//                     <div key={index} className='todo'>
-//                         {todoS(item)}
-//                         <div className='ask'>
-//                             <h3>
-//                                 <Asks dataItem={item?.ask} />
-//                             </h3>
-//                         </div>
-//                         <div className='chekboxDiv'>
-//                             {item?.options ?
-//                                 <div>
-//                                     <Checkboxs
-//                                         name={`radio-${index + 1}`}
-//                                         classN={""}
-//                                         optionsTecnology={item.options}
-//                                         selectedValue={selectedValues[`radio-${index + 1}`] || ''}
-//                                         setSelectedValue={(value: any) => handleRadioChange(`radio-${index + 1}`, value)}
-//                                     />
-//                                 </div>
-//                                 : "no"
-//                             }
-//                         </div>
-//                         <div className='answer'>
-//                             {item?.answer &&
-//                                 <Asnwers
-//                                     Asnwers={selectedValues[`radio-${index + 1}`] === item.answer}
-//                                 />
-//                             }
-//                         </div>
-//                         <div className='buttonGroup'>
-//                             <Buttons
-//                                 actionsData={() => saveAsk({
-//                                     ask: item.ask,
-//                                     options: item?.options,
-//                                     answer: item?.answer,
-//                                     myanswer: selectedValues[`radio-${index + 1}`] || undefined
-//                                 })}
-//                                 type="primary"
-//                             >
-//                                 Save Ask
-//                             </Buttons>
-//                             <Buttons
-//                                 actionsData={getItDelete}
-//                                 type="primary"
-//                                 ghost
-//                             >
-//                                 I get It delete
-//                             </Buttons>
-//                             <div className='rates'>
-//                                 <Rates />
-//                             </div>
-//                         </div>
-//                     </div>
-//                 )
-//                 ) : ""
-//             }
-//             <div className='buttonReset'>
-//                 <Buttons
-//                     actionsData={resetButton}
-//                     type=""
-//                 >
-//                     Reset
-//                 </Buttons>
-//             </div>
-//             <div className='data'></div>
-//             <pre>{JSON.stringify(selectedValues, null, 2)}</pre>
-//         </div>
-//     );
-// };
-
-// export default Maps;
-
 import { useEffect, useState } from 'react';
 import { generateNumbersRandom } from '../../utils/generateNumberRandom.utils';
 import Asks from './Asks/Asks';
@@ -242,16 +77,39 @@ const Maps = (props: any | undefined) => {
 
     }, [dataMapReturn]);
 
-
+    //levelData
     const ResetMap = (item: any | undefined | any[]) => {
+        console.log("🚀 ~ ResetMap ~ item:", item)
         resetButton();
         return true
     }
 
+
+    const getClassNameByLevel = (levelData:any) => {
+        console.log("🚀 ~ getClassNameByLevel ~ levelData:", levelData)
+        switch (levelData) {
+            case 'basic':
+                return 'todo-basic';
+            case 'middel':
+                return 'todo-middel';
+            case 'expert':
+                return 'todo-expert';
+            case 'god':
+                return 'todo-god';
+            default:
+                return 'todo-default';
+        }
+    }
+
+
+
+
+
+
     return (
         <div className='checkClass'>
             {dataMapReturn && dataMapReturn.map((item: any, index: any) => (
-                <div key={index} className='todo'>
+                <div key={index} className={`todo ${getClassNameByLevel(item?.levelData)}`}>
                     {/* {item?.options ?? ResetMap(item)} */}
 
                     {((item?.options) === undefined) ? ResetMap(item) : null}
@@ -302,7 +160,9 @@ const Maps = (props: any | undefined) => {
                             I get It delete
                         </Buttons>
                         <div className='rates'>
-                            <Rates />
+                            <Rates 
+                            levelData={item?.levelData}
+                            />
                         </div>
                     </div>
                 </div>
--- src/components/Rates/Rates.tsx
+++ src/components/Rates/Rates.tsx
@@ -1,13 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Flex, Rate } from 'antd';
 
 const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
-const Rates = () => {
+const Rates = (props: any) => {
+    const { levelData } = props
+    console.log("🚀 ~ Rates ~ levelData:", levelData)
     const [value, setValue] = useState(3);
+
+    useEffect(() => {
+        todo()
+    }, [props])
+
+    const todo = () => {
+        switch (levelData) {
+            case 'basic':
+                setValue(1);
+                break;
+            case 'middel':
+                setValue(2);
+                break;
+            case 'expert':
+                setValue(3);
+                break;
+            case 'god':
+                setValue(4);
+                break;
+            default:
+                setValue(5);
+                break;
+        }
+    }
 
     return (
         <Flex gap="small" >
-            <Rate tooltips={desc} onChange={setValue} value={value} />
+            <Rate tooltips={desc}
+            //  onChange={setValue} 
+             value={value} />
             {/* {value ? <span>{desc[value - 1]}</span> : null} */}
         </Flex>
     )
